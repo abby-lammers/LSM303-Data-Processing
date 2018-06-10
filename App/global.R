35d3b28d36000c834a65051e459ec51c6c59b5e6
thisislammers <thisislammers@gmail.com>
2018-06-10 16:09:08 -0500

    remove calibration files

--- App/global.R
+++ App/global.R
@@ -13,11 +13,8 @@ source('LSM303-file-helpers.R')
 source('plot-helpers.R')
 
 SiteFileList <- list(
-  'Akumal South' = 'AKsouth_012_20160403-0714_cleaned.csv',
-  'Akumal North' = 'B4_20141222-150317_AKnorth.csv',
-  'Casa Cenote' = 'casa_cenote_all_raw_accel_mag.csv',
-  'Odyssey' = 'OddessyCalibratedWithDateTime.csv',
-  'Calibration 1 (XY circle)' = 'calibration 1 clockwise circle true N.csv',
-  'Calibration 2 (Rotation around Z axis)' = 'calibration 2 ccw mag rotation no tilt.csv',
-  'Calibration 3 (N/S/E/W Tilt)' = 'calibration 3 plus drill.csv'
+  'Akumal South' = 'Data/AKsouth_012_20160403-0714_cleaned.csv',
+  'Akumal North' = 'Data/B4_20141222-150317_AKnorth.csv',
+  'Casa Cenote' = 'Data/casa_cenote_all_raw_accel_mag.csv',
+  'Odyssey' = 'Data/OddessyCalibratedWithDateTime.csv'
 )
